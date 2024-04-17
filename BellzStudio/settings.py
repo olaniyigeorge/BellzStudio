92db46f1173867192dc46fca7a6fa435fba9a417
unknown <olaniyigeorge77@gmail.com>
2024-04-17 13:41:02 +0100

    Initial config

--- BellzStudio/settings.py
+++ BellzStudio/settings.py
@@ -31,12 +31,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Third Part Apps
+    "channels",
+
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # My Apps
+    "main",
+    "stories",
+
 ]
 
 MIDDLEWARE = [
@@ -116,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+              "./main/**/*.{html,js}",
+              "./stories/**/*.{html,js}",
+            ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
