9d3f4ec6f69e13ebd14d9400f2fa48dc83bffcb5
unknown <olaniyigeorge77@gmail.com>
2024-04-18 02:39:27 +0100

    Extra config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
               "./stories/**/*.{html,js}",
             ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '320px',    // Extra small devices (phones)
+        'sm': '400px',    // Small devices (phones)
+        'md': '600px',    // Medium devices (tablets)
+        'lg': '900px',   // Large devices (laptops/desktops)
+        'xl': '1024px',   // Extra large devices (large laptops/desktops)
+        '2xl': '1204px',  // 2x Extra large devices
+        // Add more breakpoints as needed
+      },
+    },
   },
   plugins: [],
 }
