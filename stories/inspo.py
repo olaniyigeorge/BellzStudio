b6200cc1890cf8190ec9a05c2bf83ccc3164fc67
unknown <olaniyigeorge77@gmail.com>
2024-04-18 02:36:26 +0100

    App inspo templates

---
+++ stories/inspo.py
@@ -0,0 +1,35 @@
+
+
+# ------ Democraty  ------
+
+demo_craty_inspirations = [
+    {
+        "link": "https://twitter.com/magattew",
+        "name": "Magatte Wade",
+        "message": "Democratic processes like elections/voting should be more seamless and fictionless.",
+        "date": "November 12, 2020",
+        "source": "Twitter",    
+    },
+    {
+        "link": "https://twitter.com/asemota",
+        "name": "Victor Asemota",
+        "message": "Democratic processes like elections/voting should be more seamless and fictionless.",
+        "date": "November 12, 2020",
+        "source": "Twitter",    
+    },
+    {
+        "link": "https://twitter.com/balajis",
+        "name": "Balaji Srinivasan",
+        "message": "Democratic processes like elections/voting should be more seamless and fictionless.",
+        "date": "November 12, 2020",
+        "source": "Twitter",    
+    },
+    {
+        "link": "https://twitter.com/xniyi",
+        "name": "Abiri O.N",
+        "message": "Democratic processes like elections/voting should be more seamless and fictionless.",
+        "date": "November 12, 2020",
+        "source": "Twitter",    
+    },
+
+]
