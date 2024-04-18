bfb395ae206a1f33dae16f56a684c36e65fe69fe
unknown <olaniyigeorge77@gmail.com>
2024-04-18 02:35:44 +0100

    URLS for democraty mission, home and election page

---
+++ stories/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import *
+
+app_name = "stories"
+
+
+urlpatterns = [
+    path("", index, name="stories_index"),
+
+    # ------ DemoCraty ------
+    path("dc/", DemoCratyIndex, name="dc_Index"),
+    path("dc/home", DemoCratyDemo, name="dc_home"),
+    path("dc/<int:id>", ElectionDetails, name="election_details"),
+
+]   
