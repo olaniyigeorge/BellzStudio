97c785beb379a2a5830bf8fe5b7c6a5256e176d0
unknown <olaniyigeorge77@gmail.com>
2024-04-23 07:29:28 +0100

    Typo

--- BellzStudio/urls.py
+++ BellzStudio/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('main.urls')),
     path("dev-stories/", include('stories.urls')),
-    # path("", include('main.urls')),
+    # path("blog/", include('blog.urls')),
 
     
 ] + static(settings.STATIC_URL)
