d31a920aec67ce12a25db1be6d035802deedb7ba
unknown <olaniyigeorge77@gmail.com>
2024-05-04 21:38:22 +0100

    About us url path

--- main/urls.py
+++ main/urls.py
@@ -9,6 +9,7 @@ app_name = "main"
 
 urlpatterns = [
     path('', index, name="index"),
+    path("about-us/", about, name="about-us"),
     path("contact/", contact, name="contact"),
     path("contact/<str:destination>/", contactType, name="contact_type")
 ]
