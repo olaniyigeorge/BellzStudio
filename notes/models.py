aced50988913993a48b64e18674670fd8e2ef700
unknown <olaniyigeorge77@gmail.com>
2024-05-04 21:37:32 +0100

    Note and ideatag models

--- notes/models.py
+++ notes/models.py
@@ -1,6 +1,15 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 
 
+class IdeaTag(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class Note(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=120)
+    text = models.TextField(null=False, blank=False)
+    tags = models.ManyToManyField(IdeaTag)
 
