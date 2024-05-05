fb0997151cb912cbc85e87dc031369fbcba8afd6
unknown <olaniyigeorge77@gmail.com>
2024-05-05 14:00:39 +0100

    New urls

--- notes/urls.py
+++ notes/urls.py
@@ -5,5 +5,9 @@ from .views import *
 app_name = "notes"
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("write/", NewNote, name="write-note"),
+    # path("<uuid:noteID>/", Note, name="note"),
+    path("<str:date>/", NoteDate, name="notes-by-date"),
+    path("idea-tags/<uuid:id>/", IdeaNotes, name="notes-by-idea"),
 ]
