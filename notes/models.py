3d65bf2b4ae4ef30f8040335177e57c391999877
unknown <olaniyigeorge77@gmail.com>
2024-05-05 14:02:10 +0100

    Updated Notes model and IdeaTag to include ide descriptio n and uuid

--- notes/models.py
+++ notes/models.py
@@ -4,12 +4,37 @@ import uuid
 
 
 class IdeaTag(models.Model):
+    #slug = models.SlugField()
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=150)
+
+    description = models.TextField(null=True, blank=True)
+
+
+    def save(self, *args, **kwargs):
+        self.name = f"#{self.name}"
+
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.name
+    
+
+    def my_notes(self):
+        return self.on_notes.all()
+
 
 
 class Note(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=120)
     text = models.TextField(null=False, blank=False)
-    tags = models.ManyToManyField(IdeaTag)
+    tags = models.ManyToManyField(IdeaTag, related_name='on_notes')
+
+
+
+    written_at = models.DateTimeField(auto_now_add=True)
+
+
 
