efe0303edd777ac3031a3032d4d00200f40931fa
unknown <olaniyigeorge77@gmail.com>
2024-05-10 01:36:14 +0100

    Rduced md breakpoint to 600

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       screens: {
         // 'xs': '320px',    // Extra small devices (phones)
         'sm': '450px',    // Small devices (phones)
-        'md': '850px',    // Medium devices (tablets)
+        'md': '600px',    // Medium devices (tablets)
         'lg': '900px',   // Large devices (laptops/desktops)
         'xl': '1024px',   // Extra large devices (large laptops/desktops)
         'xxl': '1204px',  // 2x Extra large devices
