b188dc2024cc9532cd72ea91d3700086c464c074
unknown <olaniyigeorge77@gmail.com>
2024-05-10 01:30:47 +0100

    Updated screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,15 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'xs': '320px',    // Extra small devices (phones)
-        'sm': '400px',    // Small devices (phones)
-        'md': '600px',    // Medium devices (tablets)
+        // 'xs': '320px',    // Extra small devices (phones)
+        'sm': '450px',    // Small devices (phones)
+        'md': '850px',    // Medium devices (tablets)
         'lg': '900px',   // Large devices (laptops/desktops)
         'xl': '1024px',   // Extra large devices (large laptops/desktops)
-        '2xl': '1204px',  // 2x Extra large devices
-        // Add more breakpoints as needed
+        'xxl': '1204px',  // 2x Extra large devices
+      
       },
+
     },
   },
   plugins: [],
