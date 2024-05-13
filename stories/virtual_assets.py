e2dacb5d8e08cb5bd1b140315f4468b013945c1f
unknown <olaniyigeorge77@gmail.com>
2024-05-13 14:29:27 +0100

    Virtuals and scikit learn

---
+++ stories/virtual_assets.py
@@ -0,0 +1,37 @@
+url = "https://www.sportybet.com/ng/sport/vFootball/sv:category:202120001/sv:league:1"
+
+
+instant = "https://www.sportybet.com/ng/instant-virtuals/"
+
+
+PL_TEAMS = [
+                'Arsenal', 'Aston Villa', 'Brentford', 'Brighton & Hove Albion', 
+                'Burnley', 'Chelsea', 'Crystal Palace', 'Everton', 
+                'Leeds United', 'Leicester City', 'Liverpool', 'Manchester City', 
+                'Manchester United', 'Newcastle United', 'Norwich City', 'Southampton', 
+                'Tottenham Hotspur', 'Watford', 'West Ham United', 'Wolverhampton Wanderers'
+            ]
+
+EPL_TEAMS_ABBR = [
+                    'ARS', 'AST', 'BRE', 'BHA', 
+                    'BUR', 'CHE', 'CRY', 'EVE', 
+                    'LDU', 'LEI', 'LIV', 'MCI', 
+                    'MUN', 'NEW', 'NOR', 'SOU', 
+                    'TOT', 'WAT', 'WHU', 'WOL'
+                ]
+
+VIRTUAL_EPL_TEAMS = [
+                        'Arsenal', 'Aston Villa', 'Brighton & Hove Albion', 'Bournemouth FC',
+                        'Brentford', 'Burnley', 'Chelsea', 'Crystal Palace', 
+                        'Everton', 'FOR', 'Fulham FC',  'Liverpool', 
+                        'Luton Town', 'Manchester City', 'Manchester United', 'Newcastle United',
+                        'Sheffield United', 'Tottenham Hotspur', 'West Ham United', 'Wolverhampton Wanderers'
+                    ]
+
+VIRTUAL_EPL_TEAMS_ABBR = [
+                            'ARS', 'AST', 'BHA', 'BOU',  
+                            'BRE', 'BUR', 'CHE', 'CRY', 
+                            'EVE', 'FOR', 'FUL', 'LIV', 
+                            'LUT', 'MCI', 'MUN', 'NEW', 
+                            'SHU', 'TOT', 'WHU', 'WOL'
+                        ]
