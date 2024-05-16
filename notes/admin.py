869f4a4b5d8632a73dd4f564aa98efecebb7c5d8
unknown <olaniyigeorge77@gmail.com>
2024-05-16 19:54:24 +0100

    Switched uuid to slug

--- notes/admin.py
+++ notes/admin.py
@@ -5,13 +5,13 @@ from .models import Note, IdeaTag
 
 @admin.register(IdeaTag)
 class IdeaTagAdmin(admin.ModelAdmin):
-    list_display = ["id", "name", "description" ]
+    list_display = ["slug", "name", "description" ]
     list_filter = ["name"]
     search_fields = ["name"]
 
 
 @admin.register(Note)
 class NoteAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "written_at"]
+    list_display = ["slug", "title", "written_at"]
     list_filter = ["tags", "written_at"]
     search_fields = ["title", "text", ]
