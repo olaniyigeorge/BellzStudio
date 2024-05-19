b5fc9657ef9cc23a9926129fca5613d97bdd23dd
unknown <olaniyigeorge77@gmail.com>
2024-05-19 18:13:43 +0100

    Added NotePrivacy admin panels

--- notes/admin.py
+++ notes/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Note, IdeaTag
+from .models import Note, IdeaTag, NotePrivacy
 # Register your models here.
 
 
@@ -15,3 +15,10 @@ class NoteAdmin(admin.ModelAdmin):
     list_display = ["slug", "title", "written_at"]
     list_filter = ["tags", "written_at"]
     search_fields = ["title", "text", ]
+
+
+@admin.register(NotePrivacy)
+class NotePrivacyAdmin(admin.ModelAdmin):
+    list_display = ["name", "level"]
+    list_filter = ["level"]
+    search_fields = [ ]
