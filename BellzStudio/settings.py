9372609ed5daf3c6735f94de1cbf178740707bb1
unknown <olaniyigeorge77@gmail.com>
2024-06-15 19:36:17 +0100

    Fix

--- BellzStudio/settings.py
+++ BellzStudio/settings.py
@@ -23,7 +23,7 @@ import dj_database_url
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i@(jb)9@2j62kw0_y-y)+urixz)lte3or(u81o157)3%3n9dzh"
+SECRET_KEY = "django-insecure-i@(jb)9@2j62kw0_y-y)+urixz)lte3or(u81o157)3%3n9d zh"
 
 # Deployment Environment  --- "STAGE" | "DEV"   -----  DEBUG is True for both
 ENVT = "STAGE"
