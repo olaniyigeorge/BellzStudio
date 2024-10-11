12c4b7540e677e4f77d22aac0e68084f3d21a602
Olaniyi George <olaniyigeorge77@gmail.com>
2024-10-11 21:02:18 +0100

    Static dir

--- BellzStudio/settings.py
+++ BellzStudio/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # if not DEBUG:
-STATICFILES_DIRS= os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS= os.path.join(BASE_DIR, 'static'),
 STATIC_URL = '/static/' 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
